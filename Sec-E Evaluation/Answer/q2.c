971a4411460c7abdfe8d12b6557901f2dcb1d820
heipartho05 <heipartho72@gmail.com>
2023-01-19 15:31:53 +0600

    q2 added

---
+++ Sec-E Evaluation/Answer/q2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void main()
+{
+    int i, j, n;
+
+    printf("Enter nth term: ");
+    scanf("%d", &n);
+
+    int sum = 0;
+
+    if (n % 2 == 0)
+    {
+        for (i = n, j = n - 1; i >= 2, j >= 1; i -= 2, j -= 2)
+        {
+            sum += (i * (j * j));
+
+            printf("%d.%d^2 + ", i, j);
+        }
+        printf("= %d", sum);
+    }
+
+    else
+    {
+        int init_val = n * n;
+        
+        printf("%d^2 + ", n);
+
+        for (i = n - 1, j = n - 2; i >= 2, j >= 1; i -= 2, j -= 2)
+        {
+            sum += (i * (j * j));
+
+            printf("%d.%d^2 + ", i, j);
+        }
+
+        printf("= %d", sum + init_val);
+    }
+}
