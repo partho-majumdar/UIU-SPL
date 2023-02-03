2133c41f2654dd43d77a89ce548c26ee4de4e71f
heipartho05 <heipartho72@gmail.com>
2023-02-03 21:16:03 +0600

    q9 added

---
+++ Basic Introductory Problems/Solution/q9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void main()
+{
+    double value1;
+    value1 = 3.14;
+
+    bool value2;
+    value2 = true;
+
+    printf("The double value: %e\n", value1);
+    printf("The boolean value: %d", value2);
+}
