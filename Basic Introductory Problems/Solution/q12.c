53be0bb961f16b30fa7b29fcd4224d4a0c823e20
heipartho05 <heipartho72@gmail.com>
2023-02-03 21:52:04 +0600

    q12 added

---
+++ Basic Introductory Problems/Solution/q12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    const float PI = 3.14;
+    const float Golden_Ratio = 1.62;
+
+    printf("The value of pi: %.2f\n", PI);
+    printf("The value of golden ratio: %.2f", Golden_Ratio);
+}
