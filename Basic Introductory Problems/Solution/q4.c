e6bbb22fe87410c0b2f845ece68ac3b17edd9061
heipartho05 <heipartho72@gmail.com>
2023-02-03 18:22:31 +0600

    q4 added

---
+++ Basic Introductory Problems/Solution/q4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #define PI 3.1416
+
+void main()
+{
+    // const float PI = 3.1416;
+    int val1 = 5;
+    float val2 = 3.141593;
+    char ch = 'a';
+
+    printf("The integer value: %d\n", val1);
+    printf("The floating point value: %f\n", val2);
+    printf("The character value: %c\n", ch);
+}
