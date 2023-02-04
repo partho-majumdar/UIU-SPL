a4c9f81c94b9ed3c877808f4da436e87647a6c5d
heipartho05 <heipartho72@gmail.com>
2023-02-04 11:00:25 +0600

    q2 added

---
+++ Loop Related Problems/Solution/q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+
+    printf("Enter nth term: ");
+    scanf("%d", &n);
+
+    for (int i = 1, j = 1; i <= n; i++, j += 2)
+    {
+        printf("%d ", j);
+    }
+}
