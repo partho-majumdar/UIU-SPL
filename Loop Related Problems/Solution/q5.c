b5c15d556d60464d361d9c74d7a25c200d10328b
heipartho05 <heipartho72@gmail.com>
2023-02-04 11:38:00 +0600

    q5 added

---
+++ Loop Related Problems/Solution/q5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void main()
+{
+    int x, y;
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    printf("Enter value of y: ");
+    scanf("%d", &y);
+
+    if (x < y)
+    {
+        for (int i = x; i <= y; i++)
+        {
+            if (i < y)
+            {
+                printf("%d ", i * i);
+            }
+
+            else if (i == y)
+            {
+                printf("Reached!");
+            }
+        }
+    }
+
+    else if (x > y)
+    {
+        for (int i = x; i >= y; i--)
+        {
+            if (i > y)
+            {
+                printf("%d ", i * i);
+            }
+
+            else if (i == y)
+            {
+                printf("Reached!");
+            }
+        }
+    }
+
+    else
+    {
+        printf("Reached!");
+    }
+}
