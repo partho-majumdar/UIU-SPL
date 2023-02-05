16acf983ebd36f2f2f5614ccdf5158aa9063733c
heipartho05 <heipartho72@gmail.com>
2023-02-05 21:48:31 +0600

    q5 added

---
+++ Nested Loop Related Problems/Solution/q5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+
+    printf("Enter row number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1, k = n; j <= i; j++, k--)
+        {
+            printf("%d ", k);
+        }
+
+        printf("\n");
+    }
+}
