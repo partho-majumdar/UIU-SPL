2cde4a88fc695d75268898897a4a46707b32b5c3
heipartho05 <heipartho72@gmail.com>
2023-02-06 15:29:01 +0600

    q4 added

---
+++ 1D Array Related Problems/Solution/q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    int arr[100];
+
+    printf("Enter how many number you want to take as input: ");
+    scanf("%d", &n);
+
+    // take input
+    for (int i = 0; i < n; i++)
+    {
+        printf("arr[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    // print
+    int sum = 0;
+    for (int j = 0; j < n; j++)
+    {
+        if (j % 2 == 0)
+        {
+            sum += arr[j];
+        }
+    }
+
+    printf("Ans: %d", sum);
+}
