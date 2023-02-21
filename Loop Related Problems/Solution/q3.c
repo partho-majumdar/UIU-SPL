d156a02c37b5f8de81881cbe6c5eed958b04f164
heipartho05 <heipartho72@gmail.com>
2023-02-21 10:11:52 +0600

    remove some variable

--- Loop Related Problems/Solution/q3.c
+++ Loop Related Problems/Solution/q3.c
@@ -7,19 +7,16 @@ void main()
     printf("Enter nth term: ");
     scanf("%d", &n);
 
-    int j = 1;
-    int k = 0;
-
     for (int i = 1; i <= n; i++)
     {
         if (i % 2 == 0)
         {
-            printf("%d ", k);
+            printf("%d ", 0);
         }
 
         else
         {
-            printf("%d ", j);
+            printf("%d ", 1);
         }
     }
 }
