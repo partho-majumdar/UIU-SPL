82ed80d591b66ced3aef84c1942266d5cc3a355f
heipartho05 <heipartho72@gmail.com>
2023-03-07 02:33:15 +0600

    add new ans

--- Nested Loop Related Problems/Solution/q16.c
+++ Nested Loop Related Problems/Solution/q16.c
@@ -3,74 +3,24 @@
 void main()
 {
     int n;
-
-    printf("Enter row number: ");
+    printf("Enter how many row(odd) you want to print: ");
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++)
     {
-        if (i == (n + 1) / 2)
+        for (int j = 1; j <= n; j++)
         {
-            // space
-            for (int j = 1; j <= n - i; j++)
-            {
-                printf("_");
-            }
-
-            // star
-            for (int k = 1; k <= 1; k++)
+            if (i == j || j + i == n + 1)
             {
                 printf("*");
             }
 
-            // space
-            for (int j = 1; j <= n - i; j++)
+            else
             {
                 printf("_");
             }
-
-            printf("\n");
         }
 
-        else
-        {
-            // space
-            int space_start1 = i < (n + 1) / 2 ? i - 1 : n - i;
-
-            for (int j = 1; j <= space_start1; j++)
-            {
-                printf("_");
-            }
-
-            // star
-            for (int k = 1; k <= 1; k++)
-            {
-                printf("*");
-            }
-
-            // space
-            int space_start2 = i > (n + 1) / 2 ? 2 * i - n - 2 : n - 2 * i;
-
-            for (int j = 1; j <= space_start2; j++)
-            {
-                printf("_");
-            }
-
-            // star
-            for (int k = 1; k <= 1; k++)
-            {
-                printf("*");
-            }
-
-            // space
-            int space_start3 = i < (n + 1) / 2 ? i - 1 : n - i;
-
-            for (int j = 1; j <= space_start3; j++)
-            {
-                printf("_");
-            }
-
-            printf("\n");
-        }
+        printf("\n");
     }
 }
