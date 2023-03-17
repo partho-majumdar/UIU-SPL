274658dde71f391628f4897c8615a6c657d413d1
heipartho05 <heipartho72@gmail.com>
2023-03-17 11:57:59 +0600

    modify q15

--- 1D Array Related Problems/Solution/q15.c
+++ 1D Array Related Problems/Solution/q15.c
@@ -2,77 +2,63 @@
 
 void main()
 {
-    int arr1[100], arr2[100], ans[100];
+    int arr1[100], arr2[200];
+    int m, n;
 
-    int n, m, k = 0;
+    printf("Enter total number of element in arr1: ");
+    scanf("%d", &m);
 
-    printf("Enter total number of element in first array: ");
-    scanf("%d", &n);
-
-    // take first array input
-    printf("Enter first array value: \n");
-    for (int i = 0; i <= n - 1; i++)
+    for (int i = 0; i < m; i++)
     {
         printf("arr[%d]: ", i);
         scanf("%d", &arr1[i]);
     }
 
-    printf("\nEnter total number of element in second array: ");
-    scanf("%d", &m);
+    printf("\nEnter total number of element in arr2: ");
+    scanf("%d", &n);
 
-    // take second array input
-    printf("\nEnter second array value: \n");
-    for (int i = 0; i <= m - 1; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("arr[%d]: ", i);
         scanf("%d", &arr2[i]);
     }
 
-    // copy arr1's element in ans
-    int i, j;
-    for (i = 0; i <= n - 1; i++)
-    {
-        // repeated element is not allowed so we check is any value repeated
-        for (j = 0; j <= k - 1; j++)
-        {
-            if (ans[j] == arr1[i])
-            {
-                break;
-            }
-        }
+    int ans[100];
 
-        // if not repeated then store value in set
-        if (j == k)
-        {
-            ans[k] = arr1[i];
-            k++;
-        }
+    int k = 0;
+
+    // Loop through each element in arr1 and add it to ans
+    for (int i = 0; i < m; i++)
+    {
+        ans[k] = arr1[i];
+        k++;
     }
 
-    // copy arr2's element in ans
-    for (i = 0; i <= m - 1; i++)
+    // Loop through each element in arr2
+    for (int i = 0; i < n; i++)
     {
-        // repeated element is not allowed so we check is any value repeated
-        for (j = 0; j <= k - 1; j++)
+        int flag = 1;
+
+        // Loop through each element in arr1
+        for (int j = 0; j < m; j++)
         {
-            if (ans[j] == arr2[i])
+            if (arr2[i] == arr1[j])
             {
+                flag = 0;
                 break;
             }
         }
 
-        // if not repeated then store value in set
-        if (j == k)
+        if (flag == 1)
         {
             ans[k] = arr2[i];
             k++;
         }
     }
 
-    // print ans
-    printf("\nHere is the ans: \n");
-    for (int i = 0; i <= k - 1; i++)
+    printf("\nSet union: ");
+    for (int i = 0; i < k; i++)
     {
-        printf("%d  ", ans[i]);
+        printf("%d ", ans[i]);
     }
 }
