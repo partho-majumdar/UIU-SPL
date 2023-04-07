03839f720e182e55c92ac50c4a08ed14ee3ca8b3
heipartho05 <heipartho72@gmail.com>
2023-04-07 16:54:52 +0600

    q19 added

---
+++ Function Related Problem/Solution/q19.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int GeneratePrime(int);
+int isPrime(int);
+
+void main()
+{
+    int n;
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    GeneratePrime(n);
+}
+
+int GeneratePrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+        }
+    }
+}
+
+int isPrime(int n)
+{
+    int flag = 1;
+
+    if (n == 0 || n == 1)
+    {
+        return 0;
+    }
+
+    else
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag == 1)
+        {
+            return 1;
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+}
