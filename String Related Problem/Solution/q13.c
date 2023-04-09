d9f451ca1867a48ff8b53f323e93aa2fda85e6a9
heipartho05 <heipartho72@gmail.com>
2023-04-10 00:08:39 +0600

    add new code

--- String Related Problem/Solution/q13.c
+++ String Related Problem/Solution/q13.c
@@ -27,3 +27,45 @@ void main()
 
     puts(str);
 }
+
+/*
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    printf("Enter the string: ");
+    gets(str);
+
+    int length = strlen(str);
+
+    char str2[100];
+    int i, j = 0;
+
+    for (i = 0; i < length; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            str2[j++] = str[i]; // storing in new array
+
+            while (str[i] == str[i + 1]) // not storing duplicate vowels
+            {
+                i++; // skipping repetitive vowels
+            }
+        }
+
+        else
+        {
+            str2[j++] = str[i]; // for storing consonants
+        }
+    }
+    str2[j] = '\0';
+    puts(str2);
+    return 0;
+}
+
+--> This code remove duplicate vowel
+
+*/
