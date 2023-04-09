081ff079f26b9e1ad7c6e0cea29eddac3499f1cb
heipartho05 <heipartho72@gmail.com>
2023-04-09 19:02:55 +0600

    q3 added

--- Structure Related Problem/Solution/q3.c
+++ Structure Related Problem/Solution/q3.c
@@ -11,10 +11,4 @@ typedef struct
 void main()
 {
     Student s1;
-
-    strcpy(s1.name, "Jinku");
-    strcpy(s1.id, "011222266");
-    s1.cgpa = 3.91;
-
-    printf("Name: %s\nID: %s\nCGPA: %.2f", s1.name, s1.id, s1.cgpa);
 }
