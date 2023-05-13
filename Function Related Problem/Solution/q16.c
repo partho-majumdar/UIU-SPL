bbec7d5679baf6cefbaae035dcc0b15f9c7d1d08
heipartho05 <heipartho72@gmail.com>
2023-05-13 16:52:19 +0600

    modify

--- Function Related Problem/Solution/q16.c
+++ Function Related Problem/Solution/q16.c
@@ -34,7 +34,7 @@ int *multiplyArray(int *arr, int n)
 
     for (int i = 0; i < n; i++)
     {
-        newArray[i] = *(arr + i) * 2;
+        newArray[i] = arr[i] * 2;
         // *(newArray + i) = *(arr + i) * 2;
     }
 
